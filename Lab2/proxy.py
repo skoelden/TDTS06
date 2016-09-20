111fec6675148b225502dea5097b90e03db9d46c
Filip Östman <filos433@student.liu.se>
2016-09-20 20:27:55 +0200

    Added functionality for choosing port number

--- Lab2/proxy.py
+++ Lab2/proxy.py
@@ -1,6 +1,7 @@
 import socket
 import re
 import os
+import sys
 
 class easy_socket:
 
@@ -116,9 +117,24 @@ def get_host(get_request):
     return host
 
 def main():
+    #port = sys.argv[1]
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind(('localhost', 3490))
+
+    # Set the port, valid ports are between port_min and port_max
+    port_min = 1024
+    port_max = 65535
+    if len(sys.argv) < 2:
+        port = 3490
+        print("No port given, default port: {}".format(port))
+    elif int(sys.argv[1]) >= 1024 and int(sys.argv[1]) <= 100000:
+        port = int(sys.argv[1])
+        print("Port: {}".format(port))
+    else:
+        port = 3490
+        print("Invalid port, default port: {}".format(port))
+
+    serversocket.bind(('localhost', port))
     serversocket.listen(5)
 
     while 1:
