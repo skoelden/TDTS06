623b1e190984dfee60e7728ec036584ecc3cbb03
Erik Sköld <skold.erik@gmail.com>
2016-09-21 15:42:58 +0200

    Progress...

--- Lab2/proxy.py
+++ Lab2/proxy.py
@@ -30,31 +30,74 @@ class easy_socket:
             total_sent = total_sent + sent
 
     def receive_http_response(self):
-        chunks = []
-        bytes_read = 0
 
-        chunk = self.sock.recv(2048)
-        print(chunk)
-        chunks.append(chunk)
-        bytes_read = bytes_read + len(chunk)
+        part = self.sock.recv(2048)
+        if re.search(r'HTTP/\d.\d 30[14]', part):
+            return part
 
-        m_cl = re.search(r'content-length: ([^\s]+)', chunk.lower())
-        m_te = re.search(r'transfer-encoding: ([^\s]+)', chunk.lower())
+        m_cl = re.search(r'content-length: ([^\s]+)', part.lower())
+        m_te = re.search(r'transfer-encoding: ([^\s]+)', part.lower())
+
         if m_cl is not None:
             # Header has content length
+            parts = []
+            bytes_read = 0
+
+            parts.append(part)
+            bytes_read = bytes_read + len(part)
+
             content_length = int(m_cl.group(1))
 
             if content_length > bytes_read:
                 while bytes_read < content_length:
-                    chunk = self.sock.recv(min(content_length - bytes_read, 2048))
-                    chunks.append(chunk)
-                    bytes_read = bytes_read + len(chunk)
+                    part = self.sock.recv(min(content_length - bytes_read, 2048))
+                    parts.append(part)
+                    bytes_read = bytes_read + len(part)
+
+            return ''.join(parts)
 
         elif m_te is not None:
-            #print(chunk)
-            return
+            if m_te.group(1).lower() != 'chunked':
+                print("AAAARRRGHHHHHHH, this ain't no transfer-encoding i know about!")
+                return
+            else:
+                unchunkified_response = []
+                # Find end of header
+                m_eoh = re.search(r'(\r\n\r\n)', part)
+                header_length = m_eoh.end(1)
+                unchunkified_response.append(part[:header_length])
 
-        return ''.join(chunks)
+                part = part[header_length:]
+                m_chunk_header = re.match(r'([a-f0-9]*)\r\n', part)
+                part = part[m_chunk_header.end(0):]
+
+                content_length = 0
+                #for i in range(1,3):
+                while  m_chunk_header.group(1) != "":
+                    chunk_size = int(m_chunk_header.group(1), 16)
+                    content_length += chunk_size
+
+                    if len(part) < chunk_size:
+                        chunk = part
+
+                        while True:
+                            tmp = self.sock.recv(2048)
+
+                            if len(chunk) + len(tmp) > chunk_size:
+                                break
+
+                            chunk += tmp
+
+                        part = chunk + tmp # part now contatins the full chunk
+
+                    unchunkified_response.append(part[:chunk_size])
+                    part = part[chunk_size:]
+                    m_chunk_header = re.match(r'([a-f0-9]*)\r\n', part)
+
+                response = ''.join(unchunkified_response)
+                response = re.sub(r'[Tt]ransfer-[Ee]ncoding: chunked', 'Content-length: {}'.format(content_length), response)
+                #print(response)
+                return response
 
     def recv(self, num):
         self.sock.recv(num)
@@ -62,48 +105,61 @@ class easy_socket:
     def getpeername(self):
         self.sock.getpeername()
 
+    def settimeout(self, num):
+        self.sock.settimeout(num)
+
 def request_handler(s):
     get_request = s.recv(2048)
 
     client_to_proxy_socket = easy_socket(sock=s)
 
-    print(get_request)
-    if get_request[0:3] != "GET":
-        #This is not a GET request, drop it
-        print("AAAARRRGHHHHHHH, this ain't no GET request!")
-        print(get_request)
-        client_to_proxy_socket.shutdown()
-        client_to_proxy_socket.close()
-        return
+    while True:
+        #print(get_request)
+        if get_request[0:3] != "GET":
+            #This is not a GET request, drop it
+            print("AAAARRRGHHHHHHH, this ain't no GET request!")
+            print(get_request)
+            client_to_proxy_socket.shutdown()
+            client_to_proxy_socket.close()
+            return
 
-    # Search header for blocked content
+        # Search header for blocked content
 
-    host = get_host(get_request)
-    print("GET request for {}".format(host))
+        host = get_host(get_request)
+        print("GET request for {}".format(host))
 
-    proxy_to_server_socket = easy_socket()
-    proxy_to_server_socket.connect(host=host, port=80)
+        proxy_to_server_socket = easy_socket()
+        proxy_to_server_socket.connect(host=host, port=80)
 
-    proxy_to_server_socket.send(get_request)
-    http_response = proxy_to_server_socket.receive_http_response()
-    proxy_to_server_socket.shutdown()
-    proxy_to_server_socket.close()
+        proxy_to_server_socket.send(get_request)
+        http_response = proxy_to_server_socket.receive_http_response()
+        proxy_to_server_socket.shutdown()
+        proxy_to_server_socket.close()
 
-    http_version = get_http_version(get_request)
-    if http_version == '1.0':
         client_to_proxy_socket.send(http_response)
-        client_to_proxy_socket.shutdown()
-        client_to_proxy_socket.close()
-        return
-    else:
-        client_to_proxy_socket.send(http_response)
-        client_to_proxy_socket.shutdown()
-        client_to_proxy_socket.close()
-        return
-        # Check connection type
-        # Mabye, get timeout value
-        # Mabye, set timer
-        # wait
+
+        http_version = get_http_version(get_request)
+        if http_version == '1.0':
+            client_to_proxy_socket.shutdown()
+            client_to_proxy_socket.close()
+            return
+        else:
+            client_to_proxy_socket.send(http_response)
+            m_conn = re.search(r'[Cc]onnection: (\w+-\w+)', http_response)
+
+            if m_conn != None:
+                if m_conn.group(1).lower() == 'keep-alive':
+                    client_to_proxy_socket.settimeout(10)
+                    try:
+                        get_request = s.recv(2048)
+                    except socket.timeout:
+                        client_to_proxy_socket.shutdown()
+                        client_to_proxy_socket.close()
+                        return
+                else:
+                    client_to_proxy_socket.shutdown()
+                    client_to_proxy_socket.close()
+                    return
 
 
 def get_http_version(get_request):
@@ -143,13 +199,13 @@ def main():
         print("Connected to {}".format(address))
 
         # Fork and pass it to the request_handler
-        #pid = os.fork()
-        #if pid == 0:
-            #serversocket.close()
-        request_handler(s)
-         #   break
-        #else:
-            #s.close()
+        pid = os.fork()
+        if pid == 0:
+            serversocket.close()
+            request_handler(s)
+            break
+        else:
+            s.close()
 
 if __name__ == "__main__":
     main()
